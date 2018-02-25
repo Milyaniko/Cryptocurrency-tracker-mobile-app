682e0d37fa6ad3be931e8fdb791eac82accdd2f3
Nikolay Milya <Milyaniko@gmail.com>
2018-02-25 21:59:57 +0700

    Code is updated
    The commented code has been removed

--- src/Actions/FetchCoin.js
+++ src/Actions/FetchCoin.js
@@ -2,23 +2,6 @@ import { apiBaseURL } from '../Utils/Constants';
 import axios from 'axios';
 import { FETCH_DATA, FETCH_DATA_ERROR, FETCH_DATA_SUCCESS } from '../Utils/ActionTypes';
 
-// let FetchData = () => {
-//     return dispatch  => {
-
-//         dispatch({ type: FETCH_DATA })
-        
-//         fetch(`${apiBaseURL}/v1/ticker/?limit=10`)
-//             .then((response) => response.json())
-//             .then((dataCoin) => {
-//                 dispatch({ type: FETCH_DATA_SUCCESS, payload: dataCoin.data })
-//                 console.log(dataCoin.data);
-//             })
-//             .catch((error) => {
-//                 dispatch({ type: FETCH_DATA_ERROR, payload: error.data })
-//             });
-//     }
-// };
-
    const fetchCoin = () => {
        return dispatch => {
          dispatch({ type: FETCH_DATA })
@@ -34,4 +17,5 @@ import { FETCH_DATA, FETCH_DATA_ERROR, FETCH_DATA_SUCCESS } from '../Utils/Actio
 
        }
    }
+   
 export default fetchCoin;
